d2ee20f98429410d547ba0b7ca2bb94b8d006942
User <justin.the@gmail.com>
2019-10-17 09:56:13 +0700

    masking done!

--- code/find_contour.py
+++ code/find_contour.py
@@ -1,6 +1,11 @@
 # USAGE
 # python find_contour.py -i ../data/NDVI_2.jpg
 
+# change image to binary
+# get contour
+# find boundary - paddy field frame
+# mask
+
 # import packages
 import numpy as np
 import argparse
@@ -10,6 +15,7 @@ import imutils
 # references
 # https://www.learnopencv.com/applications-of-foreground-background-separation-with-semantic-segmentation/
 # https://www.codepasta.com/computer-vision/2016/11/06/background-segmentation-removal-with-opencv.html
+
 
 def detect_edge(channel):
 	sobelX = cv2.Sobel(channel, cv2.CV_64F, dx=1, dy=0)
@@ -21,25 +27,6 @@ def detect_edge(channel):
 	sobel = cv2.addWeighted(sobelX, 0.5, sobelY, 0.5, 0)
 	return sobel
 
-def get_significant_contours(edgeImg):
-	cnts = cv2.findContours(edgeImg, cv2.RETR_LIST, cv2.CHAIN_APPROX_SIMPLE)
-	cnts = imutils.grab_contours(cnts)
-	too_small = edgeImg.size * 0.1
-	significants = []
-	# print("C :{}".format(type(cnts)))
-	for (i, c) in enumerate(cnts):
-		area = cv2.contourArea(c)
-		# print("Area {}".format(area))
-		# print("shape: {}".format(c.shape))
-		# print("C: {}".format(c))
-		if area > too_small:
-			significants.append([c, area])
-
-		significants.sort(key=lambda r: r[1])
-				
-	# return [x[0] for x in significants]
-	return significants
-
 # construct argument parse and parse command line
 ap =  argparse.ArgumentParser()
 ap.add_argument("-i", "--image", required=True, help="Path to the image")
@@ -49,58 +36,102 @@ args = vars(ap.parse_args())
 image = cv2.imread(args["image"])
 gray = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
 
-blurred = cv2.GaussianBlur(gray, (9, 9), 0)
+blurred = cv2.GaussianBlur(gray, (3, 3), 0)
 edgeImg = detect_edge(blurred)
-# edgeImg = detect_edge(gray)
 
 
-
-significant_contour = get_significant_contours(edgeImg)
-print("Found {} contours".format(len(significant_contour)))
-
-# get smallest contour - the actual paddy field we're interested in
-# cv2.drawContours(image, significant_contour[0], -1, (255, 0, 0), 2)
-
-# contour smoothing
-# epsilon = 0.1 * cv2.arcLength(significant_contour[0], True)
-# approx = cv2.approxPolyDP(significant_contour[0], epsilon, True)
-# significant_contour[0] = approx
-
-# for i in significant_contour:
-# 	print(i[0].shape)
+# binary
+(t, binary) = cv2.threshold(edgeImg, 20, 255, cv2.THRESH_BINARY_INV)
+# cv2.imshow("Binary", binary)
 
 # dilate
-dilated = significant_contour[0][0].copy().astype(np.uint8)
-dilated = cv2.GaussianBlur(dilated, (9, 9), 0)
-# cv2.dilate(dilated, None, 3)
-# dilated = dilated[:,0]
-# print(a.dtype)
-# print(image.dtype)
-print("Dilated :{}".format(dilated.shape))
-print("Image: {}".format(image.shape))
-print(gray.shape)
+dilated = binary.copy()
+for i in range(0, 3):
+	cv2.dilate(dilated, None, iterations = i + 1)
 
-ne = []
-for i in dilated:
-	# print(i[0])
-	ne.append(i[0])
+# # morphological gradient - to get the outside boundary
+# kernel = cv2.getStructuringElement(cv2.MORPH_RECT, (3, 3))
+# opening = cv2.morphologyEx(dilated, cv2.MORPH_OPEN, kernel)
 
-# print(len(ne))
+# cv2.imshow("Opening", opening)
+# cv2.imshow("Edge", edgeImg)
+# cv2.imshow("Dilated", dilated)
+# cv2.waitKey(0)
 
-# print(dilated[0][0].shape)
-# print(dilated[0])
+cnts = cv2.findContours(dilated, cv2.RETR_LIST, cv2.CHAIN_APPROX_SIMPLE)
+cnts = imutils.grab_contours(cnts)
+# cv2.drawContours(image, cnts, -1, (255, 0, 0), 2)
+
+mask_boundaries = []
+
+for (i, c) in enumerate(cnts):
+	# print("Drawing contour #{}".format(i + 1))
+	# cv2.drawContours(image, [c], -1, (255, 0, 0), 2)
+	area = cv2.contourArea(c)
+	if area > 300000.0 and area < 500000.0:
+		cv2.drawContours(image, [c], -1, (255, 0, 0), 2)
+		# print("Drawing contour #{}".format(i + 1))
+		print("Area {}".format(area))
+		mask_boundaries.append(c)
+		# print(c.dtype)
+
+# cv2.imshow("Contours", image)
+# cv2.waitKey(0)
+
 # mask
-mask = edgeImg.copy()
-mask[mask > 0] = 0
-# cv2.rectangle(mask, (2,4), (150, 200), (255, 255,255), -1)
-cv2.fillConvexPoly(mask, np.int32(ne), 255)
-mask = np.logical_not(mask)
-image[mask] = 0
+mask = np.zeros(image.shape[:2], dtype="uint8")
+cv2.fillPoly(mask, [mask_boundaries[0]], 255)
+masked = cv2.bitwise_and(image, image, mask=mask)
 
-
-# show output
-cv2.imshow("All Contours", image)
+cv2.imshow("Masked", masked)
 cv2.waitKey(0)
+
+# significant_contour = get_significant_contours(edgeImg)
+# print("Found {} contours".format(len(significant_contour)))
+
+# # get smallest contour - the actual paddy field we're interested in
+# # cv2.drawContours(image, significant_contour[0], -1, (255, 0, 0), 2)
+
+# # contour smoothing
+# # epsilon = 0.1 * cv2.arcLength(significant_contour[0], True)
+# # approx = cv2.approxPolyDP(significant_contour[0], epsilon, True)
+# # significant_contour[0] = approx
+
+# # for i in significant_contour:
+# # 	print(i[0].shape)
+
+# # dilate
+# dilated = significant_contour[0][0].copy().astype(np.uint8)
+# dilated = cv2.GaussianBlur(dilated, (9, 9), 0)
+# # cv2.dilate(dilated, None, 3)
+# # dilated = dilated[:,0]
+# # print(a.dtype)
+# # print(image.dtype)
+# print("Dilated :{}".format(dilated.shape))
+# print("Image: {}".format(image.shape))
+# print(gray.shape)
+
+# ne = []
+# for i in dilated:
+# 	# print(i[0])
+# 	ne.append(i[0])
+
+# # print(len(ne))
+
+# # print(dilated[0][0].shape)
+# # print(dilated[0])
+# # mask
+# mask = edgeImg.copy()
+# mask[mask > 0] = 0
+# # cv2.rectangle(mask, (2,4), (150, 200), (255, 255,255), -1)
+# cv2.fillConvexPoly(mask, np.int32(ne), 255)
+# mask = np.logical_not(mask)
+# image[mask] = 0
+
+
+# # show output
+# cv2.imshow("All Contours", image)
+# cv2.waitKey(0)
 
 # # first parameter = image to threshold
 # # second parameter = threshold value
